42eb59578910050a29290e03cb44c6eb84185f61
afuhflynn <flyinnsafuh@gmail.com>
2025-12-31 07:43:08 +0100

    feat: add footer section with restart typing test button

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,13 @@
 export const FooterSection = () => {
-  return <div>FooterSection</div>;
+  return (
+    <div className="flex w-full border border-x-0 border-b-0 border-t-border lg:pt-8 pt-6 items-end justify-center">
+      <button
+        className="font-semibold py-5 px-6 rounded-[12px] bg-neutral-800 text-xl hover:bg-neutral-800/80 transition-colors duration-75 flex items-center gap-4"
+        // onClick={() => setTypingState("NEW")}
+      >
+        <span>Restart Test</span>
+        <img src="/images/icon-restart.svg" alt="Restart Icon" />
+      </button>
+    </div>
+  );
 };

9b7d3b0be8cea9f00d7f15db4629c2eb9d4b2710
afuhflynn <flyinnsafuh@gmail.com>
2025-12-31 09:59:06 +0100

    feat: add confetti image and make results component completely reusable

--- src/components/results-component.tsx
+++ src/components/results-component.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useTypingStore } from "../zustand";
 
 interface props {
@@ -8,19 +9,30 @@ export const ResultsComponenet = ({ status }: props) => {
   const { test } = useTypingStore();
   const { wpm, accuracy, chars } = test;
   return (
-    <div className="flex flex-col items-center w-full md:gap-12 gap-10">
-      <div className="rounded-full h-36 w-36 bg-green-500/10 flex items-center justify-center">
-        <div className="h-25 w-25 rounded-full bg-green-500/20 flex items-center justify-center">
-          <img src="/images/icon-completed.svg" alt="Restart Icon" />
+    <div className="flex flex-col items-center w-full md:gap-12 gap-10 relative flex-1">
+      {status !== "NEW_PERSONAL_BEST" ? (
+        <div className="rounded-full h-36 w-36 bg-green-500/10 flex items-center justify-center">
+          <div className="h-25 w-25 rounded-full bg-green-500/20 flex items-center justify-center">
+            <img src="/images/icon-completed.svg" alt="Completed Icon" />
+          </div>
         </div>
-      </div>
+      ) : (
+        <img src="/images/icon-new-pb.svg" alt="New Personal Best Icon" />
+      )}
       <div className="text-center flex flex-col gap-2.5">
         <h1 className="font-bold! md:text-[40px] text-[24px]">
-          Baseline Established!
+          {status === "FIRST_TEST"
+            ? "Baseline Established!"
+            : status === "NEW_PERSONAL_BEST"
+            ? "High Score Smashed!"
+            : "Test Complete!"}
         </h1>
         <p className="md:text-[20px] text-[16px] text-neutral-400">
-          You&apos;ve set the bar. Now the real challenge begins—time to beat
-          it.
+          {status === "FIRST_TEST"
+            ? "You've set the bar. Now the real challenge begins—time to beat it."
+            : status === "NEW_PERSONAL_BEST"
+            ? "You’re getting faster. That was incredible typing."
+            : "Solid run. Keep pushing to beat your high score."}
         </p>
       </div>
       <div className="flex md:items-center gap-6 flex-col md:flex-row w-full md:justify-center">
@@ -46,17 +58,40 @@ export const ResultsComponenet = ({ status }: props) => {
           </h2>
         </div>
       </div>
-      <button
-        className="font-semibold py-5 px-6 rounded-[12px] text-neutral-900 bg-neutral-0 text-xl hover:bg-neutral-0/80 transition-colors duration-75 flex items-center gap-4"
-        // onClick={restartTyping}
-      >
-        <span>Beat This Score</span>
-        <img
-          className="invert-1"
-          src="/images/icon-undo.svg"
-          alt="Restart Icon"
-        />
-      </button>
+      <Link to="/" className="decoration-0">
+        <button
+          className="font-semibold py-5 px-6 rounded-[12px] text-neutral-900 bg-neutral-0 text-xl hover:bg-neutral-0/80 transition-colors duration-75 flex items-center gap-4 md:mt-8! mt-2!"
+          // onClick={restartTyping}
+        >
+          <span>{status !== "RESULTS" ? "Beat This Score" : "Go Again"}</span>
+          <img
+            className="invert-1"
+            src="/images/icon-undo.svg"
+            alt="Restart Icon"
+          />
+        </button>
+      </Link>
+      <img
+        src="/images/pattern-star-1.svg"
+        alt="Star 1 Icon"
+        className={`absolute bottom-0 right-0 ${
+          status === "NEW_PERSONAL_BEST" ? "hidden" : ""
+        }`}
+      />
+      <img
+        src="/images/pattern-star-2.svg"
+        alt="Star 2 Icon"
+        className={`absolute top-0 left-0 ${
+          status === "NEW_PERSONAL_BEST" ? "hidden" : ""
+        }`}
+      />
+      <img
+        src="/images/pattern-confetti.svg"
+        alt="Confetti Pattern Icon"
+        className={`fixed bottom-0 self-center w-full object-fill ${
+          status === "NEW_PERSONAL_BEST" ? "" : "hidden"
+        }`}
+      />
     </div>
   );
 };
--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -5,7 +5,7 @@ const Results = () => {
   return (
     <div className="flex flex-col h-full w-full padding lg:gap-16 md:gap-20 gap-8 overflow-auto overflow-x-hidden">
       <Header />
-      <ResultsComponenet status="FIRST_TEST" />
+      <ResultsComponenet status="NEW_PERSONAL_BEST" />
     </div>
   );
 };
